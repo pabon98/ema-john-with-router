b7df86fec8d00185981640ebe9d432f0e493ddb1
pabon98 <hasinanjumpabon@gmail.com>
2021-10-01 20:02:20 +0600

    added

---
+++ src/Hooks/useProducts.js
@@ -0,0 +1,14 @@
+import {useState} from "react"
+import {useEffect} from "react"
+
+const useProducts =()=>{
+    const [products,setProducts]= useState()
+    useEffect( ()=>{
+fetch('products.JSON')
+.then(res=>res.json())
+.then(data=> setProducts(data))
+    },[])
+    //return necessary things
+    return [products,setProducts]
+}
+export default useProducts
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import useProducts from '../../Hooks/useProducts';
+import useCart from '../../Hooks/useCart'
+import Cart from '../Cart/Cart'
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const OrderReview = () => {
+    const [products, setProducts] = useProducts();
+    const[cart,setCart] = useCart(products)
+    const handleRemove = key=>{
+        const newCart = cart.filter(product=> product.key !==key)
+        setCart(newCart)
+        removeFromDb(key)
+    }
     return (
-        <div>
-            <h2>This is Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product=> <ReviewItem
+                    key={product.key}
+                        product={product}
+                        handleRemove={handleRemove}
+                        ></ReviewItem>)
+                }
+        </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+            
         </div>
     );
 };
